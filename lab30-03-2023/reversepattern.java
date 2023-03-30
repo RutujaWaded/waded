14c4a1106970c388ddec335bd89b51bb9f6fbca8
RutujaWaded <129392093+RutujaWaded@users.noreply.github.com>
2023-03-30 19:22:20 +0530

    Create reversepattern.java

---
+++ lab30-03-2023/reversepattern.java
@@ -0,0 +1,20 @@
+//INVERTED HALF PYRAMID
+import java.util.Scanner;
+public class reversepattern {
+
+  public static void main(String[] args) 
+  {
+    int rows;
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the no of rows:");
+    rows=sc.nextInt();
+
+    for ( int i = rows; i >= 1; i++)
+    {
+      for (int j = 1; j <= i; j++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
